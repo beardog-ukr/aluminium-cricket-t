9775e2088911955cbe2d4538b1c5ff24c2142de5
beardog-ukr <you@example.com>
2019-03-05 22:15:41 +0200

    Basic red square moving (hamster style)

--- main.lua
+++ main.lua
@@ -1,23 +1,34 @@
 
+Object = require "thirdparty/classic-master/classic"
+
 require "src/red_square"
 local redSquare = RedSquare(10, 20)
 
 -- ===========================================================================
 -- ===========================================================================
 
-function love.load(arg)  
-  redSquare.x=13
+function love.keypressed(key)
+  redSquare:processKeyPressed(key)
 end
 
-function love.update(diffTime)
-  --  
+function love.load(arg)  
+  --
 end
 
 local palette = {}
 palette.emptyScreen = {0.5, 0.5, 0.8, 1} -- 
 
 function love.draw()
+  local cww = love.graphics.getWidth(); -- current window width
+  local cwh = love.graphics.getHeight();-- current window height 
 
   love.graphics.setColor(palette.emptyScreen)
-  love.graphics.rectangle("fill", 0, 0, 10, 20)
+  love.graphics.rectangle("fill", 0, 0, cww, cwh)
+
+  --
+  redSquare:drawSelf()
+end
+
+function love.update(diffTime)
+  redSquare:processUpdate(diffTime)  
 end
--- src/red_square.lua
+++ src/red_square.lua
@@ -1,8 +1,49 @@
-local Object = require "../thirdparty/classic-master/classic"
 
 RedSquare = Object:extend()
 
 function RedSquare:new(x, y)
   self.x = x or 0
   self.y = y or 0
+
+  self.color = {0.8,0.1,0.1,1}
+  self.size = 20
+
+  self.speed = 300 -- px per second
+
+  self.direction = "none"
+end
+
+function RedSquare:processKeyPressed(keyPressed)
+  -- TODO: use constants here
+  if ("right"==keyPressed) then
+    self.direction = "right"
+  elseif ("left"==keyPressed) then
+    self.direction = "left"
+  elseif ("down"==keyPressed) then
+    self.direction = "down"
+  elseif ("up"==keyPressed) then
+    self.direction = "up"
+  end
+end
+
+function RedSquare:processUpdate(diffTime)
+  local coordinateDiff = (self.speed * diffTime)
+  if ("right"==self.direction) then
+    self.x = self.x + coordinateDiff
+  elseif ("left"==self.direction) then
+    self.x = self.x - coordinateDiff
+  elseif ("down"==self.direction) then
+    self.y = self.y + coordinateDiff
+  elseif ("up"==self.direction) then
+    self.y = self.y - coordinateDiff
+  end  
+end
+
+function RedSquare:drawSelf()
+  love.graphics.setColor(self.color)
+  love.graphics.rectangle("fill", self.x, self.y, self.size, self.size)
+end
+
+function RedSquare:boo()
+  print("boo " .. self.x)
 end
