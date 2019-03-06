b75478ce53ceeb5585c4b533714899a70c79fffa
beardog-ukr <you@example.com>
2019-03-06 11:17:04 +0200

    Unsignificant change

--- main.lua
+++ main.lua
@@ -15,36 +15,36 @@ function love.keypressed(key)
   redSquare:processKeyPressed(key)
 end
 
-function love.load(arg)  
-  local success = love.window.setMode(ds.windowWidth, ds.windowHeight, 
+function love.load(arg)
+  local success = love.window.setMode(ds.windowWidth, ds.windowHeight,
                                       {resizable=false, minwidth=100, minheight=300})
   if (not success) then
     print("Failed to set window mode")
   end
 
-  redSquare:setGameArea(ds.largeFrameSize, ds.largeFrameSize, 
+  redSquare:setGameArea(ds.largeFrameSize, ds.largeFrameSize,
                         ds.gameAreaWidth, ds.gameAreaHeight)
 end
 
 function love.draw()
   local cww = love.graphics.getWidth(); -- current window width
-  local cwh = love.graphics.getHeight();-- current window height 
+  local cwh = love.graphics.getHeight();-- current window height
 
   love.graphics.setColor(ds.palette.emptyScreen)
   love.graphics.rectangle("fill", 0, 0, cww, cwh)
 
   --
   love.graphics.setColor(ds.palette.gameAreaBorder)
-  love.graphics.rectangle("fill", 
+  love.graphics.rectangle("fill",
                           ds.largeFrameSize - ds.smallFrameSize, ds.largeFrameSize - ds.smallFrameSize,
                           ds.gameAreaWidth + ds.smallFrameSize*2, ds.smallFrameSize)
-  love.graphics.rectangle("fill", 
+  love.graphics.rectangle("fill",
                           ds.largeFrameSize - ds.smallFrameSize, ds.largeFrameSize + ds.gameAreaHeight,
                           ds.gameAreaWidth + ds.smallFrameSize*2, ds.smallFrameSize)
-  love.graphics.rectangle("fill", 
+  love.graphics.rectangle("fill",
                           ds.largeFrameSize - ds.smallFrameSize, ds.largeFrameSize,
                           ds.smallFrameSize, ds.gameAreaHeight)
-  love.graphics.rectangle("fill", 
+  love.graphics.rectangle("fill",
                           ds.largeFrameSize + ds.gameAreaWidth, ds.largeFrameSize,
                           ds.smallFrameSize, ds.gameAreaHeight)
   --
@@ -52,5 +52,5 @@ function love.draw()
 end
 
 function love.update(diffTime)
-  redSquare:processUpdate(diffTime)  
+  redSquare:processUpdate(diffTime)
 end
